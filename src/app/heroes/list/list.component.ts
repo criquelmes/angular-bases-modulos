d922752c5c3f76a6781d92cc553773bf6bef5f87
Christopher Riquelme <ccriquelmes@gmail.com>
2025-01-18 00:24:24 -0300

    Directiva ngFor

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
-export class ListComponent {}
+export class ListComponent {
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+}
